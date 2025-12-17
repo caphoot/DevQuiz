cb78efca92556ea57b243d45ac93abcd85dc21d4
elisemjoen <elise@mjoen.no>
2025-12-17 13:42:00 +0100

    Add avatar license info

--- DevQuiz.WebClient/src/App.vue
+++ DevQuiz.WebClient/src/App.vue
@@ -2,11 +2,13 @@
   <div class="bg-primary min-h-[100dvh] min-h-screen flex flex-col gap-4 bg-center bg-no-repeat bg-cover" :style="{ backgroundImage: `url(${bg})` }">
     <AppHeader />
     <RouterView />
+    <AvatarLicense />
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue'
+import AvatarLicense from '@/components/AvatarLicense.vue'
 import { RouterView } from 'vue-router'
 import bg from '@/assets/background/Frame 2148.png'
 </script>
